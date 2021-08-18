de49638e82f0d24e4161cc2eba7b2b4be82877b4
athira <vsathira33@gmail.com>
2021-08-18 13:21:50 +0530

    Updated the search employee functionality.
    - Earlier we could search employee only by using name.
    - Updated the search with the following:
        - Search employee by name
        - Search employee by age
        - Search employee by salary
        - Search employee by gender

--- employee.py
+++ employee.py
@@ -65,20 +65,113 @@ def delete_employee():
 
 
 def search_employee_by_name():
-    name = input("Enter employee name: ")
+    name = input("\tEnter employee name: ")
     emp_present = False
     if name:
         for emp_id, employee in employees.items():
             if employee["name"] == name:
                 emp_present = True
-                print(f"\tEmployee id: \t {emp_id}")
+                print(f"\n\tEmployee id: \t {emp_id}")
                 for key in employee:
                     print(f"\t{key}: \t {employee[key]}")
         if not emp_present:
-            print(name + " is not an employee.")
+            print("\t" + name + " is not an employee.")
 
     else:
-        print("No valid input received. Try again")
+        print("\tNo valid input received. Try again")
+
+
+def search_employee_by_age():
+    age = input("\tEnter employee age: ")
+    emp_present = False
+    if age:
+        for emp_id, employee in employees.items():
+            if employee["age"] == age:
+                emp_present = True
+                print(f"\n\tEmployee id: \t {emp_id}")
+                for key in employee:
+                    print(f"\t{key}: \t {employee[key]}")
+        if not emp_present:
+            print("\t" + age + " is not an age of any employee.")
+
+    else:
+        print("\tNo valid input received. Try again")
+
+
+def search_employee_by_salary():
+    salary = input("\tEnter employee salary: ")
+    emp_present = False
+    if salary:
+        for emp_id, employee in employees.items():
+            if employee["salary"] == salary:
+                emp_present = True
+                print(f"\n\tEmployee id: \t {emp_id}")
+                for key in employee:
+                    print(f"\t{key}: \t {employee[key]}")
+        if not emp_present:
+            print("\t" + salary + " is not salary of any employee.")
+
+    else:
+        print("\tNo valid input received. Try again")
+
+
+def search_employee_by_gender():
+    gender = input("\tEnter employee gender: ")
+    emp_present = False
+    if gender:
+        for emp_id, employee in employees.items():
+            if employee["gender"] == gender:
+                emp_present = True
+                print(f"\n\tEmployee id: \t {emp_id}")
+                for key in employee:
+                    print(f"\t{key}: \t {employee[key]}")
+        if not emp_present:
+            print("\t" + gender + " is not gender of any employee.")
+
+    else:
+        print("\tNo valid input received. Try again")
+
+
+
+def search_employee_menu():
+    print("\n\t1. Search employee by name")
+    print("\t2. Search employee by age")
+    print("\t3. Search employee by salary")
+    print("\t4. Search employee by gender")
+    print("\t5. Return to previous menu")
+
+
+
+def search_employee():
+    while True:
+        search_employee_menu()
+        ch = input("\tEnter your choice: ")
+        if ch == '1':
+            # Search employee by name
+            search_employee_by_name()
+
+        elif ch == '2':
+            # Search employee by age
+            search_employee_by_age()
+
+        elif ch == '3':
+            # Search employee by salary
+            search_employee_by_salary()
+
+        elif ch == '4':
+            # Search employee by gender
+            search_employee_by_gender()
+
+        elif ch == '5':
+            # Exit
+            break
+
+        else:
+            print("\tInvalid choice!!!")
+            
+
+
+
 
 
 def display_employee():
@@ -89,7 +182,7 @@ def display_employee():
 
 
 def change_employee_menu():
-    print("\tMENU")
+    print("\n\tMENU")
     print("\t1. Change employee name")
     print("\t2. Change age")
     print("\t3. Change gender")
@@ -211,7 +304,7 @@ def change_employee_data():
 
 
 def manage_teams_menu():
-    print("\t1. Create new team")
+    print("\n\t1. Create new team")
     print("\t2. Display team")
     print("\t3. Manage particular team")
     print("\t4. Delete team")
@@ -236,7 +329,7 @@ def display_teams():
 
 
 def manage_particular_team_menu():
-    print("\t\t1. Add employee")
+    print("\n\t\t1. Add employee")
     print("\t\t2. Delete employee")
     print("\t\t3. List employee")
     print("\t\t4. Rename team")
@@ -360,7 +453,7 @@ def add_organization():
 
 
 def manage_organization_menu():
-    print("\t1. Change organization name")
+    print("\n\t1. Change organization name")
     print("\t2. Change organization email")
     print("\t3. Display organization")
     print("\t4. Return to previous menu")
@@ -437,7 +530,7 @@ while True:
 
     elif choice == 3:
         #Search Employee
-        search_employee_by_name()
+        search_employee()
     
     elif choice == 4:
         # Change employee Data
