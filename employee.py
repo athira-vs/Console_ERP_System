801d82df87b5bae3b87c654663491365fc190303
athira <vsathira33@gmail.com>
2021-08-18 12:30:03 +0530

    Added manage organization functionalities.
    - Included the following:
        - Add organization name and email on first set up.
        - Manage organization
            - Edit organization name
            - Edit organization email
            - Display organization details

--- employee.py
+++ employee.py
@@ -1,3 +1,8 @@
+employees = {} # Dict for employee details
+teams = {} # Dict for employees team details
+org = {} # Dict for organization details
+
+
 def print_main_menu():
     print("\n\n_________MENU_________")
     print("1. Add Employee")
@@ -6,7 +11,8 @@ def print_main_menu():
     print("4. Change Employee Data")
     print("5. Display Employees")
     print("6. Manage Teams")
-    print("7. Exit")
+    print("7. Manage organization")
+    print("8. Exit")
 
 def add_employee():
     emp_id = input("Enter employee id: ")
@@ -343,9 +349,74 @@ def manage_teams():
                 break
 
 
-employees = {}
-teams = {}
+def add_organization():
+    name = input("Enter organization name: ")
+    email = input("Enter organization email: ")
+    if name and email:
+        org["name"] = name
+        org["email"] = email
+    else:
+        print("Cannot leave name or email empty. Try again.")
+
+
+def manage_organization_menu():
+    print("\t1. Change organization name")
+    print("\t2. Change organization email")
+    print("\t3. Display organization")
+    print("\t4. Return to previous menu")
+
+
+def change_org_name():
+    new_name = input("Enter new organization name: ")
+    if new_name:
+        org["name"] = new_name
+    else:
+        print("Cannot leave name empty. Try again.")
+
+
+def change_org_email():
+    new_email = input("Enter new organization email: ")
+    if new_email:
+        org["email"] = new_email
+    else:
+        print("Cannot leave email empty. Try again.")
+
+def display_organization():
+    for key, value in org.items():
+        print("\t{} => {}".format(key,value))
+
+
+
+def manage_organization():
+    while True:
+        manage_organization_menu()
+        ch = input("Enter your choice: ")
+        if ch == '1':
+            # Change organization name
+            change_org_name()        
+
+        elif ch == '2':
+            # Change organization email
+            change_org_email()
+
+        elif ch == '3':
+            # Dispaly organization
+            display_organization()
+
+        elif ch == '4':
+            # Exit
+            break
+
+        else:
+            print("Invalid choice!!!")
+
+
+
+
+# Add organization on first setup
+add_organization()
 while True:
+    
     print_main_menu()
 
     choice = input("Enter your choice: ")
@@ -379,8 +450,12 @@ while True:
     elif choice == 6:
         #Manage Teams
         manage_teams()
-        
+
     elif choice == 7:
+        #Manage organization
+        manage_organization()
+        
+    elif choice == 8:
         # Exit
         break
     else:
